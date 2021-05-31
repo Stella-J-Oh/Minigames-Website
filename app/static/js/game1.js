06ff86bb431d4afae692cb96c9a8656c8f355476
constanceche <constancechen64@gmail.com>
2021-05-30 21:47:13 -0400

    attempted game one

--- app/static/js/game1.js
+++ app/static/js/game1.js
@@ -2,3 +2,33 @@
 // SoftDev
 // P3: ArRESTed Development, JuSt in Time
 // 2021-04-21
+
+var canvas = document.getElementById("slate");
+var ctx = canvas.getContext("2d");
+
+// Box width
+var bw = 400;
+// Box height
+var bh = 400;
+// Padding
+var p = 10;
+
+function drawBoard(){
+    for (var x = 0; x <= bw; x += 40) {
+        ctx.moveTo(0.5 + x + p, p);
+        ctx.lineTo(0.5 + x + p, bh + p);
+    }
+
+    for (var x = 0; x <= bh; x += 40) {
+        ctx.moveTo(p, 0.5 + x + p);
+        ctx.lineTo(bw + p, 0.5 + x + p);
+    }
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    console.log ("success");
+}
+
+drawBoard();
+
+
+
